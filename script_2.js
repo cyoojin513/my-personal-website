ef05dd0cf56ffa5189d127015839163a530a9fd7
yoojin <yoojin9005@gmail.com>
2022-06-19 05:34:09 -0400

    Done with third room

--- script_2.js
+++ script_2.js
@@ -3,6 +3,6 @@ const link = "third_room.html"
 
 element.addEventListener('click', function() {
     window.alert('Cats:   Hello');
-    window.alert('Cats:   If you are my friend, you could come in');
+    window.alert('Cats:   If you\'re my friend, you can come in');
     location.href = link;
 })
